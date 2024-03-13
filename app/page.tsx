b0ae39c20d601820cfb74abc54202e273040cf8b
Luke Davies <47165253+LukeDavies12@users.noreply.github.com>
2024-03-13 11:00:55 -0500

    theming blue new york shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,9 @@ export default function LandingPage() {
     <div className="flex flex-col min-h-screen">
       <header className="px-4 lg:px-6 h-14 flex items-center">
         <Link className="flex items-center justify-center" href="#">
-          <MountainIcon className="h-6 w-6" />
-          <span className="sr-only">Acme Inc</span>
+          <span className="ml-2 text-lg font-bold text-neutral-900 dark:text-neutral-50">
+            Approvlify
+          </span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
           <Link
